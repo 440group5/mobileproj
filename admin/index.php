9c4124412e5c6b3e308fd872c257bb2d1afd929d
OpenShift guest <jrlloyd@cinci.rr.com>
2014-03-11 13:12:58 -0400

    Started Admin Control Panel

--- admin/index.php
+++ admin/index.php
@@ -3,8 +3,9 @@
   if($_SESSION['type'] != 1)
       echo "You are not an admin!";
   else {
-      echo "<a href='users.php'>Edit Users</a>";
-      echo "<a href='spots.php'>Edit Spots</a>";
+      echo "<a href='users.php'>Edit Users</a><br/>";
+      echo "<a href='spots.php'>Edit Spots</a><br/>";
+      echo "<a href='../logout.php'>Log Out</a><br/>";
   }
 
 ?>
---
+++ admin/spots.php
@@ -0,0 +1,52 @@
+<?php 
+  session_start();
+  require("../config.php");
+  
+  if($_SESSION['type'] != 1)
+      echo "You are not an admin!";
+  else {
+      ?>
+      <h1>Lots &amp; Spots</h1>
+      <form action="spots.php" method="post">
+        LOT:<select name="lot">
+            <option value="A" selected>A</option>
+            <option value="C">C</option>
+            <option value="K">K</option>
+            <option value="L">L</option>
+            <option value="M">M</option>
+        </select>
+        <input type="submit" value="submit">
+      </form>
+      <br/>
+      <?php
+      $lot=$_POST["lot"];
+      isset($lot) || $lot='A'; //set lot or default to A
+      
+      $db = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+    
+      if($db->connect_errno > 0) {
+        die("Could not connect to database");
+      }
+      
+      $query = "select * from spots where lot='".$lot."';";
+      $spots = $db->query($query);
+
+      echo"<table>";
+      echo"<tr><th>ID</th><th>LOT</th><th>Availabiltiy</th><th>User</th></tr>";
+      
+      while($spot = $spots->fetch_array(MYSQLI_ASSOC) ) {
+          if($spot['occupied'] == 1)
+            $availablity = "Occupied";
+          elseif(time() < $spot['expired'])
+            $availability = "Reserved";
+          else
+            $availability = "Available";
+            
+          echo"<tr><td>".$spot['id']."</td><td>".$spot['lot']."</td><td>".$availability."</td><td>".$spot['username']."</td></tr>";
+      }
+      echo"</table><br/>";
+      echo $spot['occupied']."<br/>";
+      echo"<a href='/admin'>Admin</a>";
+  }    
+
+?>
