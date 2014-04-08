ab2e6efcf75859fd0a0eec4bbf44cbe3520dedd8
ryanlietzenmayer <lietzenmar1@nku.edu>
2014-04-08 16:43:21 -0400

    Repush

---
+++ NKUParking/src/com/csc440/group5/nkuparking/ObjSpace.java
@@ -0,0 +1,11 @@
+package com.csc440.group5.nkuparking;
+
+public class ObjSpace
+{
+	//type student/faculty/handicapped/other
+	int spaceType;
+	//available/unavailable
+	boolean avail = true;
+	//maybe number/index
+
+}
