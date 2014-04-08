29828593c5e6987ba0b6fc7fa7efac4d9dea728c
ryanlietzenmayer <lietzenmar1@nku.edu>
2014-04-08 16:38:29 -0400

    Object lots and Spaces started, working on status page.

--- NKUParking/src/com/csc440/group5/nkuparking/StatusPage.java
+++ NKUParking/src/com/csc440/group5/nkuparking/StatusPage.java
@@ -1,10 +1,17 @@
 package com.csc440.group5.nkuparking;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class StatusPage extends Activity {
 
@@ -12,7 +19,41 @@ public class StatusPage extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_status_page);
+
+		final Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Perform action on click
+				Context context = getApplicationContext();
+				int duration = Toast.LENGTH_SHORT;
+
+				Toast toast = Toast.makeText(context, "Button pressed", duration);
+				toast.show();
+			}
+		});
 	}
+
+	/*
+	 	LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		TableRow rowView = (TableRow)inflater.inflate(R.layout.my_row, null);
+		table.addRow(rowView);
+
+	public void Table(){
+		TableLayout.LayoutParams tableParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT);
+		TableRow.LayoutParams rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+
+		TableLayout tableLayout = new TableLayout(context);
+		tableLayout.setLayoutParams(this.tableParams);
+
+		TableRow tableRow = new TableRow(context);
+		tableRow.setLayoutParams(this.tableParams);
+
+		TextView textView = new TextView(context);
+		textView.setLayoutParams(this.rowParams);
+
+		tableRow.addView(textView);
+	}*/
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -20,36 +61,36 @@ public class StatusPage extends Activity {
 		getMenuInflater().inflate(R.menu.status_page, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		
-	    // Handle presses on the action bar items
-	    switch (item.getItemId()) {
-	    
-	        case R.id.action_map:
-	        	Intent map = new Intent(this, MapPage.class);
-	    		startActivity(map);
-	            return true;
-	            
-	        case R.id.action_directions:
-	        	Intent directions = new Intent(this, DirectionsPage.class);
-	    		startActivity(directions);
-	            return true;
-	            
-	        case R.id.action_status:
-	        	Intent status = new Intent(this, StatusPage.class);
-	    		startActivity(status);
-	            return true;
-	            
-	        case R.id.action_settings:
-	        	Intent settings = new Intent(this, SettingsPage.class);
-	    		startActivity(settings);
-	            return true;
-	            
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+
+		case R.id.action_map:
+			Intent map = new Intent(this, MapPage.class);
+			startActivity(map);
+			return true;
+
+		case R.id.action_directions:
+			Intent directions = new Intent(this, DirectionsPage.class);
+			startActivity(directions);
+			return true;
+
+		case R.id.action_status:
+			Intent status = new Intent(this, StatusPage.class);
+			startActivity(status);
+			return true;
+
+		case R.id.action_settings:
+			Intent settings = new Intent(this, SettingsPage.class);
+			startActivity(settings);
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 }
